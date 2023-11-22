29af2956a201ff6fe96e5db4d8255cc1a4889aa9
kaushik <kaushikrajput0926@gmail.com>
2023-11-22 23:18:10 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
+
   return (
     <div className="navbar">
       <div className="links">
-        <Link to='/'>Shop</Link>
-        <Link to='/cart'>
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
           <i className="fa-solid fa-cart-shopping"></i>
         </Link>
       </div>
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,33 +1,38 @@
-import { React, useContext } from 'react'
-import { ShopContext } from '../../context/ShopContext'
-import { PRODUCTS } from '../../products'
-import { CartItem } from './CartItem'
+import { React, useContext } from "react";
+import { ShopContext } from "../../context/ShopContext";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./CartItem";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const navigate = useNavigate();
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
 
-  const { cartItems, getTotalCartAmount } = useContext(ShopContext)
-
-  const totalAmount = getTotalCartAmount()
+  const totalAmount = getTotalCartAmount();
 
   return (
-    <div className='cart'>
-      <div className=''>
-        <h1>Your Cart Items</h1>
+    <div className="cart">
+      <div className="">
+        <h1> Cart Items</h1>
       </div>
-      <div className='cartItems'>
+      <div className="cartItems">
         {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />
+            return <CartItem data={product} />;
           }
         })}
       </div>
-      <div className='checkout'>
-        <p>Subtotal: ${totalAmount}</p>
-        <button>Continue Shopping</button>
-        <button>Checkout</button>
-      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Total Ammount: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h2>Your cart is Empty</h2>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
